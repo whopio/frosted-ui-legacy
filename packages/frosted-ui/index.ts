985c9e38bdeaf0f505d9a877260f2f6f520ea8af
Makenna Smutz <makennasmutz@gmail.com>
2023-07-17 19:38:08 -0400

    EmptyState + story

--- packages/frosted-ui/index.ts
+++ packages/frosted-ui/index.ts
@@ -3,6 +3,7 @@ export * from './components/Badge';
 export * from './components/Banner';
 export * from './components/Button';
 export * from './components/Chip';
+export * from './components/EmptyState';
 export * from './components/Icon';
 export * from './components/IconButton';
 export * from './components/Tag';
