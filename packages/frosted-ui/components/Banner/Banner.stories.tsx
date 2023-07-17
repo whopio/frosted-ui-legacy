a331813b884d05db614fc8da1fb9055701577dcf
Makenna Smutz <makennasmutz@gmail.com>
2023-07-17 19:01:42 -0400

    Button and Banner + stories

---
+++ packages/frosted-ui/components/Banner/Banner.stories.tsx
@@ -0,0 +1,38 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import * as React from 'react';
+import { Banner, BannerColorSchemes } from './Banner';
+
+const meta: Meta<typeof Banner> = {
+  title: 'General/Banner',
+  component: Banner,
+  args: {
+    showIcon: true,
+    title: 'Some callout here',
+    ctaText: 'Learn more',
+    colorScheme: 'purple',
+    allowDismiss: false,
+  },
+};
+export default meta;
+type Story = StoryObj<typeof Banner>;
+
+export const Default: Story = {};
+
+export const Dismissable: Story = {
+  args: {
+    allowDismiss: true,
+  },
+};
+
+export const ColorSchemes: Story = {
+  render: (args) => {
+    const colorSchemes = Object.values(BannerColorSchemes);
+    return (
+      <div className="flex flex-col gap-6">
+        {colorSchemes.map((colorScheme, i) => (
+          <Banner key={i} {...args} colorScheme={colorScheme} />
+        ))}
+      </div>
+    );
+  },
+};
--- packages/frosted-ui/index.ts
+++ packages/frosted-ui/index.ts
@@ -1,5 +1,7 @@
 export * from './components/AlertBanner';
 export * from './components/Badge';
+export * from './components/Banner';
+export * from './components/Button';
 export * from './components/Icon';
 export * from './components/IconButton';
 export * from './components/Tag';
