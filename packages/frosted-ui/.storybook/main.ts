5355efc86dcab82d63b52feb5efd9c326bee3c5b
Makenna Smutz <makennasmutz@gmail.com>
2023-07-17 16:19:40 -0400

    Get tailwind running with Storybook

--- packages/frosted-ui/.storybook/main.ts
+++ packages/frosted-ui/.storybook/main.ts
@@ -1,6 +1,9 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
-  stories: ['../stories/**/*.mdx', '../stories/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: [
+    '../stories/**/*.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+  ],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
--- packages/frosted-ui/components/Badge/Badge.stories.tsx
+++ packages/frosted-ui/components/Badge/Badge.stories.tsx
@@ -1,6 +1,18 @@
-import React from 'react';
+import type { Meta, StoryObj } from '@storybook/react';
 import { Badge } from './Badge';
 
-export const BadgeStory = () => {
-  return <Badge text="Badge" />;
+const meta: Meta<typeof Badge> = {
+  component: Badge,
+};
+
+export default meta;
+type Story = StoryObj<typeof Badge>;
+
+/*
+ *👇 Render functions are a framework specific feature to allow you control on how the component renders.
+ * See https://storybook.js.org/docs/react/api/csf
+ * to learn how to use render functions.
+ */
+export const Primary: Story = {
+  render: () => <Badge text="Button" />,
 };
