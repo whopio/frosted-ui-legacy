857d6f2f2ceafa7d6b21c04ed5dab11f57ab2584
Makenna Smutz <makennasmutz@gmail.com>
2023-07-18 09:57:16 -0400

    TextButton Stories

--- packages/frosted-ui/components/TextButton/TextButton.stories.tsx
+++ packages/frosted-ui/components/TextButton/TextButton.stories.tsx
@@ -1,5 +1,12 @@
+import { faHandPointRight } from '@fortawesome/free-solid-svg-icons';
 import type { Meta, StoryObj } from '@storybook/react';
-import { TextButton } from './TextButton';
+import * as React from 'react';
+import {
+  TextButton,
+  TextButtonColorSchemes,
+  TextButtonSizes,
+  TextButtonVariants,
+} from './TextButton';
 
 const meta: Meta<typeof TextButton> = {
   title: 'General/TextButton',
@@ -16,3 +23,48 @@ export default meta;
 type Story = StoryObj<typeof TextButton>;
 
 export const Default: Story = {};
+
+export const CustomIcon: Story = {
+  args: {
+    icon: faHandPointRight,
+  },
+};
+
+export const Variants: Story = {
+  render: (args) => {
+    const variants = Object.values(TextButtonVariants);
+    return (
+      <div className="space-x-6">
+        {variants.map((variant) => (
+          <TextButton key={variant} {...args} variant={variant} />
+        ))}
+      </div>
+    );
+  },
+};
+
+export const ColorSchemes: Story = {
+  render: (args) => {
+    const colorSchemes = Object.values(TextButtonColorSchemes);
+    return (
+      <div className="space-x-6">
+        {colorSchemes.map((colorScheme) => (
+          <TextButton key={colorScheme} {...args} colorScheme={colorScheme} />
+        ))}
+      </div>
+    );
+  },
+};
+
+export const Sizes: Story = {
+  render: (args) => {
+    const sizes = Object.values(TextButtonSizes);
+    return (
+      <div className="space-x-6">
+        {sizes.map((size) => (
+          <TextButton key={size} {...args} size={size} />
+        ))}
+      </div>
+    );
+  },
+};
