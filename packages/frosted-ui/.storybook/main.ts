47bc7323eb717320cad2f584f1acaf1b5d217ca6
Makenna Smutz <makennasmutz@gmail.com>
2023-07-18 17:01:43 -0400

    upgrade to latest storybook

--- packages/frosted-ui/.storybook/main.ts
+++ packages/frosted-ui/.storybook/main.ts
@@ -1,20 +1,13 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 const config: StorybookConfig = {
-  stories: [
-    '../stories/**/*.mdx',
-    '../components/**/*.stories.@(js|jsx|ts|tsx)',
-  ],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-interactions',
-  ],
+  stories: ['../stories/**/*.mdx', '../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-interactions', '@storybook/addon-mdx-gfm'],
   framework: {
     name: '@storybook/react-vite',
-    options: {},
+    options: {}
   },
   docs: {
-    autodocs: 'tag',
-  },
+    autodocs: 'tag'
+  }
 };
 export default config;
