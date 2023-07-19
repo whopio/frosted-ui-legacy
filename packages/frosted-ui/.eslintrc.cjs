911413011d623faccc95f5663fcfb033ef6b6b93
Makenna Smutz <makennasmutz@gmail.com>
2023-07-19 10:44:51 -0400

    Update packages/frosted-ui/.eslintrc.cjs

--- packages/frosted-ui/.eslintrc.cjs
+++ packages/frosted-ui/.eslintrc.cjs
@@ -22,12 +22,6 @@ module.exports = {
   },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      {
-        allowConstantExport: true,
-      },
-    ],
     '@typescript-eslint/no-non-null-assertion': 'off',
   },
 };
