2b6e9480850872fa4c8a69e9985ebe659f2c7f0f
Makenna Smutz <makennasmutz@gmail.com>
2023-07-19 15:37:46 -0400

    Update the themes

---
+++ packages/frosted-ui/preset/themes.ts
@@ -0,0 +1,299 @@
+export const light = {
+  name: 'default',
+  transparent: 'transparent',
+  extend: {
+    colors: {
+      whop: {
+        // Neutral colors
+        black: '#0e0e0f',
+        'off-black': '#28292e',
+        'dark-gray': '#535961',
+        gray: '#888e96',
+        'stroke-dark': '#d2d1d1',
+        stroke: '#e8e8e8',
+        'hover-press': '#ebebeb',
+        hover: '#f6f6f6',
+        'hover-50': '#fbfbfc',
+        background: '#ffffff',
+
+        // Primary colors
+        primary: '#ff6243',
+        'primary-hover': '#9A2A14',
+
+        // Secondary colors
+        'field-highlight': '#625bf6',
+        'field-highlight-hover': '#4842c4',
+
+        // Accent colors
+        'success-green': '#4AB325',
+        'warning-yellow': '#e2b100',
+        'error-red': '#ff3333',
+        'blue-accent': '#16A0FB',
+        'purple-accent': '#A33AD5',
+        'lavender-accent': '#7D69F7',
+        'indigo-accent': '#271F6A',
+
+        // Brand colors
+        brands: {
+          discord: '#5865F2',
+          twitter: '#1DA1F2',
+          coinbase: '#1152F0',
+          instagram: '#DD2A7B',
+          opensea: '#2081E2',
+          paypal: '#00457C',
+          tradingview: '#2962ff',
+          telegram: '#2481cc',
+          stripe: '#635bff',
+        },
+
+        // Tag colors
+        tag: {
+          green: '#38863E',
+          'green-background': '#E6F7CF',
+          blue: '#2857B9',
+          'blue-background': '#D8F4F6',
+          gray: '#555A68',
+          'gray-background': '#EBEEF1',
+          purple: '#9C39D4',
+          'purple-background': '#F0E1F9',
+          warning: '#815016',
+          'warning-background': '#FBECB8',
+          error: '#CF1616',
+          'error-background': '#FCE5DD',
+        },
+
+        // Perk colors
+        perk: {
+          'software-start': '#FF6243',
+          'software-end': '#FFA843',
+          'discord-start': '#3E48C2',
+          'discord-end': '#5865F2',
+          'content-start': '#005EEB',
+          'content-end': '#649BEE',
+          'file-start': '#F00046',
+          'file-end': '#FD57A5',
+          'link-start': '#DA942C',
+          'link-end': '#F2C935',
+          'custom-start': '#00B400',
+          'custom-end': '#87E293',
+        },
+
+        // Gradient colors
+        gradient: {
+          'purple-start': '#8033C9',
+          'purple-middle': '#5F37B6',
+          'purple-end': '#412A8B',
+          'fire-start': '#9A0606',
+          'fire-middle': '#EF4508',
+          'fire-end': '#FEDF01',
+        },
+
+        // Misc colors
+        'fixed-black': '#ffffff',
+        'avatar-stroke': '#F8F8FA',
+        'fixed-white': '#FFFFFF',
+        'primary-button-text': '#FFFFFF',
+      },
+    },
+  },
+};
+
+export const darkOne = {
+  name: 'biz-dark-1',
+  transparent: 'transparent',
+  extend: {
+    colors: {
+      whop: {
+        // Neutral colors
+        black: '#EAEAEA',
+        'off-black': '#D3D1CE',
+        'dark-gray': '#BCB9B4',
+        gray: '#8A8682',
+        'stroke-dark': '#363636',
+        stroke: '#333333',
+        'hover-press': '#3C3C3C',
+        hover: '#282828',
+        'hover-50': '#1E1E1E',
+        background: '#191919',
+
+        // Primary colors
+        primary: '#FF6243',
+        'primary-hover': '#9A2A14',
+
+        // Secondary colors
+        'field-highlight': '#625BF6',
+        'field-highlight-hover': '#4842C4',
+
+        // Accent colors
+        'success-green': '#4AB325',
+        'warning-yellow': '#e2b100',
+        'error-red': '#ff3333',
+        'blue-accent': '#16A0FB',
+        'purple-accent': '#A33AD5',
+        'lavender-accent': '#7D69F7',
+        'indigo-accent': '#271F6A',
+
+        // Brand colors
+        brands: {
+          discord: '#5865F2',
+          twitter: '#1DA1F2',
+          coinbase: '#1152F0',
+          instagram: '#DD2A7B',
+          opensea: '#2081E2',
+          paypal: '#00457C',
+          tradingview: '#2962ff',
+          telegram: '#2481cc',
+          stripe: '#635bff',
+        },
+
+        // Tag colors
+        tag: {
+          green: '#55B036',
+          'green-background': '#25342A',
+          blue: '#3677FF',
+          'blue-background': '#1D2656',
+          gray: '#838587',
+          'gray-background': '#2D2939',
+          purple: '#A347D7',
+          'purple-background': '#362341',
+          warning: '#C7AF59',
+          'warning-background': '#342B28',
+          error: '#CF1616',
+          'error-background': '#471927',
+        },
+
+        // Perk colors
+        perk: {
+          'software-start': '#FF6243',
+          'software-end': '#FFA843',
+          'discord-start': '#3E48C2',
+          'discord-end': '#5865F2',
+          'content-start': '#005EEB',
+          'content-end': '#649BEE',
+          'file-start': '#F00046',
+          'file-end': '#FD57A5',
+          'link-start': '#DA942C',
+          'link-end': '#F2C935',
+          'custom-start': '#00B400',
+          'custom-end': '#87E293',
+        },
+
+        // Gradient colors
+        gradient: {
+          'purple-start': '#8033C9',
+          'purple-middle': '#5F37B6',
+          'purple-end': '#412A8B',
+          'fire-start': '#9A0606',
+          'fire-middle': '#EF4508',
+          'fire-end': '#FEDF01',
+        },
+
+        // Misc colors
+        'fixed-black': '#000000',
+        'avatar-stroke': '#1B1B1B',
+        'fixed-white': '#FFFFFF',
+        'primary-button-text': '#FFFFFF',
+      },
+    },
+  },
+};
+
+export const darkTwo = {
+  name: 'biz-dark-2',
+  transparent: 'transparent',
+  extend: {
+    colors: {
+      whop: {
+        // Neutral colors
+        black: '#FFFFFF',
+        'off-black': '#ECECEE',
+        'dark-gray': '#BDBEC3',
+        gray: '#83858E',
+        'stroke-dark': '#464C63',
+        stroke: '#44495E',
+        'hover-press': '#424962',
+        hover: '#383D51',
+        'hover-50': '#313648',
+        background: '#2A2D3D',
+
+        // Primary colors
+        primary: '#8966D7',
+        'primary-hover': '#7153B5',
+
+        // Secondary colors
+        'field-highlight': '#625BF6',
+        'field-highlight-hover': '#4842C4',
+
+        // Accent colors
+        'success-green': '#4AB325',
+        'warning-yellow': '#e2b100',
+        'error-red': '#ff3333',
+        'blue-accent': '#16A0FB',
+        'purple-accent': '#A33AD5',
+        'lavender-accent': '#7D69F7',
+        'indigo-accent': '#271F6A',
+
+        // Brand colors
+        brands: {
+          discord: '#5865F2',
+          twitter: '#1DA1F2',
+          coinbase: '#1152F0',
+          instagram: '#DD2A7B',
+          opensea: '#2081E2',
+          paypal: '#00457C',
+          tradingview: '#2962ff',
+          telegram: '#2481cc',
+          stripe: '#635bff',
+        },
+
+        // Tag colors
+        tag: {
+          green: '#55B036',
+          'green-background': '#2F3D3C',
+          blue: '#3677FF',
+          'blue-background': '#2D3E6A',
+          gray: '#838587',
+          'gray-background': '#393C4A',
+          purple: '#A347D7',
+          'purple-background': '#483363',
+          warning: '#C7AF59',
+          'warning-background': '#403F41',
+          error: '#CF1616',
+          'error-background': '#4E2835',
+        },
+
+        // Perk colors
+        perk: {
+          'software-start': '#FF6243',
+          'software-end': '#FFA843',
+          'discord-start': '#3E48C2',
+          'discord-end': '#5865F2',
+          'content-start': '#005EEB',
+          'content-end': '#649BEE',
+          'file-start': '#F00046',
+          'file-end': '#FD57A5',
+          'link-start': '#DA942C',
+          'link-end': '#F2C935',
+          'custom-start': '#00B400',
+          'custom-end': '#87E293',
+        },
+
+        // Gradient colors
+        gradient: {
+          'purple-start': '#8033C9',
+          'purple-middle': '#5F37B6',
+          'purple-end': '#412A8B',
+          'fire-start': '#9A0606',
+          'fire-middle': '#EF4508',
+          'fire-end': '#FEDF01',
+        },
+
+        // Misc colors
+        'fixed-black': '#000000',
+        'avatar-stroke': '#303343',
+        'fixed-white': '#FFFFFF',
+        'primary-button-text': '#FFFFFF',
+      },
+    },
+  },
+};
