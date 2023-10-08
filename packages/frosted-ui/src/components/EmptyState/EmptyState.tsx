500a4115fb61d3edb13affe880cfada1cf5a3c7b
Artur Bien <artur.bien@gmail.com>
2023-10-08 18:35:10 +0200

    EmptyState

--- packages/frosted-ui/src/components/EmptyState/EmptyState.tsx
+++ packages/frosted-ui/src/components/EmptyState/EmptyState.tsx
@@ -25,35 +25,31 @@ export const EmptyState = ({
 }: EmptyStateProps) => {
   return (
     <div
-      className={cn(
-        'border-whop-stroke-dark bg-whop-background w-full rounded-[10px] px-10 py-6',
-        { border: showBorder },
-      )}
+      className={cn('fui-EmptyState', {
+        'fui-EmptyState--show-border': showBorder,
+      })}
     >
-      <div className="mx-auto flex max-w-[400px] flex-col gap-6 text-center">
-        <div className="border-whop-stroke-dark bg-whop-background mx-auto flex animate-pulse items-center justify-center border drop-shadow-lg h-12 w-12 rounded-2xl">
-          <Icon icon={icon} className={'text-whop-gray h-6 w-6'} />
+      <div className="fui-EmptyState-layout">
+        <div className="fui-EmptyState-icon-wrapper">
+          <Icon icon={icon} className="fui-EmptyState-icon" />
         </div>
         <div>
-          <Text as="p" variant="h3" className="text-whop-off-black">
+          <Text as="p" variant="h3">
             {title}
           </Text>
-          <Text as="p" variant="body2" className="text-whop-dark-gray mt-1">
+          <Text as="p" variant="body2" className="fui-EmptyState-description">
             {description}
           </Text>
         </div>
-        <div className="mx-auto flex w-full flex-col-reverse gap-2 sm:flex-row sm:justify-center">
+        <div className="fui-EmptyState-buttons">
           {secondaryButton && (
             <Button
               variant="outline"
               colorScheme="black"
-              className="w-full sm:w-auto"
               {...secondaryButton}
             />
           )}
-          {primaryButton && (
-            <Button className="w-full sm:w-auto" {...primaryButton} />
-          )}
+          {primaryButton && <Button {...primaryButton} />}
         </div>
       </div>
     </div>
