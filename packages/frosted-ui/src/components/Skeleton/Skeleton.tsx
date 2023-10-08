10744ee1b4349d23dc047a68bc4c0c47387e7b53
Artur Bien <artur.bien@gmail.com>
2023-10-08 10:35:24 +0200

    Skeleton

--- packages/frosted-ui/src/components/Skeleton/Skeleton.tsx
+++ packages/frosted-ui/src/components/Skeleton/Skeleton.tsx
@@ -5,12 +5,7 @@ function Skeleton({
   className,
   ...props
 }: React.HTMLAttributes<HTMLDivElement>) {
-  return (
-    <div
-      className={cn('animate-pulse rounded-md bg-whop-gray/10', className)}
-      {...props}
-    />
-  );
+  return <div className={cn('fui-Skeleton', className)} {...props} />;
 }
 
 export { Skeleton };
